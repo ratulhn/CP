aa3e8f50bef25bef44f1d3af12f5a1cf676fe8e8
ratulhn <ratulhossain.bd2003@gmail.com>
2024-10-03 00:53:03 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> ordered_set;
+#define FastIO ios::sync_with_stdio(false);cin.tie(nullptr);
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << "\n";
+#include "debugging.h"
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define nl cout<<'\n'
+#define lp(i,a,n) for(int i=a; i<n; i++)
+
+const int M = 1e9+7;
+
+
+
+
+
+
+
+int main() {
+    FastIO
+    
+    
+
+
+
+
+    #ifndef ONLINE_JUDGE
+    cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+
+    return 0;
+    
+}
