c9d43df6ad37730373b67bed7ce66de3abd5c7a8
ratulhn <ratulhossain.bd2003@gmail.com>
2025-12-01 16:18:11 +0600

    Update Sum of divisors 1 to N.cpp

--- Sum of divisors 1 to N.cpp
+++ Sum of divisors 1 to N.cpp
@@ -1,11 +1,9 @@
 long long n;
 cin >> n;
 long long ans=0;
-for( long long i=2,j; i * i <= n; i++ ) {
+for( long long i=1,j; i * i <= n; i++ ) {
   j = n / i;
   ans += ( i + j ) * ( j - i + 1 ) / 2;
   ans += i * ( j - i );
 }
-ans+=n;
-ans+=(n*(n+1)/2)-1;
 cout << ans << "\n";
